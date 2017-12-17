fb2063ae1a0c6fac4189a52c3f7f2d73b2c52845
arjun <arjun.94.choudhry@gmail.com>
2017-12-17 07:53:42 -0600

    fitting the linear Regressor

--- multiple_linear_regression/multiple_linear_regression.py
+++ multiple_linear_regression/multiple_linear_regression.py
@@ -31,3 +31,8 @@ X = X[:,1:]
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+# Fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+linearRegressor = LinearRegression()
+linearRegressor = linearRegressor.fit(X_train,y_train)
