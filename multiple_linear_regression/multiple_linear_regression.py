be19ff53cb2d6b78ccf5971c7dee4611dcae7a66
arjun <arjun.94.choudhry@gmail.com>
2017-12-17 07:52:52 -0600

    data_preprocessing done

---
+++ multiple_linear_regression/multiple_linear_regression.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 03:20:21 2017
+
+@author: arjun
+"""
+
+# *************************************************************************************
+# AIM: Create a model to predict the salary of employees based on the given data      #
+# *************************************************************************************
+
+# Data Preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('50_Startups.csv')
+
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoderState = LabelEncoder()
+X[:,3] = labelEncoderState.fit_transform(X[:,3])
+
+oneHotEncoderState = OneHotEncoder(categorical_features=[3])
+X = oneHotEncoderState.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap - Libraries take care of this
+X = X[:,1:]
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
