2ee898ac06588373f4efeaf2f3ca3f00395bcaf7
arjun <arjun.94.choudhry@gmail.com>
2017-12-18 15:55:51 -0600

    Fitting the model

--- SVR/svr.py
+++ SVR/svr.py
@@ -14,3 +14,11 @@ X_scale = sc_X.fit_transform(X)
 # X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_scale = sc_y.fit_transform(y)
+
+# Fitting the SVR Model to the dataset
+from sklearn.svm import SVR
+svrRegressor = SVR(kernel = 'rbf')
+svrRegressor.fit(X_scale,y_scale)
+
+y_pred = svrRegressor.predict(X_scale)
+y_pred_actual = sc_y.inverse_transform(y_pred)
