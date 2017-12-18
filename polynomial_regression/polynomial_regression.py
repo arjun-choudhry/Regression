70333d9d52664c437a82ddfca8db2507c4447964
arjun <arjun.94.choudhry@gmail.com>
2017-12-18 12:28:06 -0600

    Making the plot continuous

--- polynomial_regression/polynomial_regression.py
+++ polynomial_regression/polynomial_regression.py
@@ -40,8 +40,12 @@ plt.ylabel("Salary")
 plt.show()
 
 # Plotting the points for visualization of Polynomial Linear Regressor
+#To make the plot continuous
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid),1)
+
 plt.scatter(X,y,color="red")
-plt.plot(X,y_pred_polynomialLinearRegression,color="blue")
+plt.plot(X_grid,linReg2.predict(polyReg.fit_transform(X_grid)),color="blue")
 plt.title("Simple Linear Regressor")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
